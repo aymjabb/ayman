161019398a7c5d3bf23546b5dc15032716904dfb
aymjabb <kkayman300@gmail.com>
2026-01-07 11:08:53 +0000

    Create seraCore.js

---
+++ modules/seraCore.js
@@ -0,0 +1,11 @@
+if (!global.SERA) {
+  global.SERA = {
+    MODE: "NORMAL", // NORMAL | DEVIL
+    OWNER: "61577861540407",
+    FEAR: {},
+    WATCH: {},
+    SILENT: {}
+  };
+}
+
+module.exports = global.SERA;
