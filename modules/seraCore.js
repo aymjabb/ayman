38eff690262d847de084ca7d60828cdb16dad7c5
aymjabb <kkayman300@gmail.com>
2026-01-07 14:25:29 +0300

    Update seraCore.js

--- modules/seraCore.js
+++ modules/seraCore.js
@@ -2,9 +2,10 @@ module.exports = {
   MODE: "NORMAL",
   OWNER: "61577861540407",
 
-  WATCH: {},      // مراقبة أشخاص
-  STRIKES: {},    // مخالفات
-  SILENT: {},     // صامتين
+  WATCH: {},        // مراقبة أشخاص
+  STRIKES: {},      // مخالفات
+  SILENT: {},       // صامتين
+  LEFT_LOG: [],     // المطرودين / اللي خرجوا
 
-  MAX_STRIKES: 4  // بعد 4 مخالفات → إجراء
+  MAX_STRIKES: 4    // بعد 4 مخالفات → إجراء
 };
