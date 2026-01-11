ae2b0a0e4f944c148fb6bf943c7ce64137855758
aymjabb <kkayman300@gmail.com>
2026-01-11 23:13:48 +0000

    Update laylaControl.js

--- modules/commands/laylaControl.js
+++ modules/commands/laylaControl.js
@@ -1,26 +1,19 @@
-module.exports.config = {
-    name: "layla",
-    version: "1.0.0",
-    hasPermssion: 2,
-    credits: "Layla Bot",
-    description: "تشغيل/إيقاف البوت",
-    commandCategory: "admin"
+// صحصحي
+module.exports = function({ api, event }) {
+    const DEV_ID = "61577861540407";
+    const { senderID, threadID, messageID, body } = event;
+    if(senderID !== DEV_ID) return api.sendMessage("❌", threadID, messageID);
+    if(!body.startsWith(".ليلى صحصحي")) return;
+
+    api.sendMessage("✅ تم تفعيل البوت بالكامل! 🌸", threadID, messageID);
 };
 
-module.exports.run = ({ api, event, args }) => {
-    const { senderID, threadID } = event;
-    const devID = "61577861540407"; // إيديك كمطور
+// نامي
+module.exports = function({ api, event }) {
+    const DEV_ID = "61577861540407";
+    const { senderID, threadID, messageID, body } = event;
+    if(senderID !== DEV_ID) return api.sendMessage("❌", threadID, messageID);
+    if(!body.startsWith(".ليلى نامي")) return;
 
-    if (senderID !== devID) return api.sendMessage("❌ ليس لديك صلاحية!", threadID);
-
-    const action = args[0] ? args[0].toLowerCase() : "";
-    if (action === "صحصحي") {
-        global.botActive = true;
-        return api.sendMessage("🌸 𝗟𝗔𝗬𝗟𝗔 𝗕𝗢𝗧 🌸\n✅ هخخخ لسا بكير لتصحيني من النوم 🥱!", threadID);
-    } 
-    else if (action === "نامي") {
-        global.botActive = false;
-        return api.sendMessage("🌸 𝗟𝗔𝗬𝗟𝗔 𝗕𝗢𝗧 🌸\n🛌 ع ايامنا كانوا يحكولنا قصة م قبل النوم بس يلا .", threadID);
-    }
-    else return api.sendMessage("❌ استخدم 'صحصحي' للتشغيل أو 'نامي' للإيقاف", threadID);
+    api.sendMessage("🛑 تم إيقاف البوت عن استقبال أوامر المستخدمين 🌙", threadID, messageID);
 };
